7a8a5472ae3d67ad9e7bf5648db192b0e53a0e48
horizon <89808589+autumnstudios@users.noreply.github.com>
2023-05-20 16:36:01 -0600

    Initial commit

---
+++ src/main/java/com/autumnstudios/libraries/foundation/Foundation.kt
@@ -0,0 +1,13 @@
+package com.autumnstudios.libraries.foundation
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Foundation : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
